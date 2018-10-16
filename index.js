fd716d5f30895a583b40d17f9180eea40d903f92
Kiril Gaydarov <kvg1@alu.ua.es>
2018-10-16 14:42:17 +0200

    Añadido listar todos los coches y listar coche por ID

--- index.js
+++ index.js
@@ -4,21 +4,39 @@ var app = express()
 var knex = require('knex')({
     client: 'sqlite3',
     connection: {
-      filename: "./ejemplo.db"
+      filename: "./coches.db"
     }
 });
 
 //"capa" web (no aparecen referencias al API de Knex)
-app.get("/users", function(pet, resp){
-    listarUsuarios(function(datos){
+app.get("/", function(pet, resp) {
+    resp.redirect('/cars')
+})
+
+app.get("/cars", function(pet, resp){
+    listarCoches(function(datos){
         resp.send(datos)
     })
 
 })
 
+app.get("/cars/:id", function(pet, resp){
+    listarCocheID(function(datos){
+        resp.send(datos)
+    }, pet.params.id)
+
+})
+
 //"capa" de acceso a datos (no aparecen referencias al API de Express)
-function listarUsuarios(callback) {
-    knex.select().from('Usuarios')
+function listarCoches(callback) {
+    knex.select().from('Coches')
+    .then(function(datos){
+      callback(datos)
+    })
+}
+
+function listarCocheID(callback, id) {
+    knex.select().from('Coches').where('id', id)
     .then(function(datos){
       callback(datos)
     })
