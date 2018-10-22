dab6fc812148c9bb2ded333ba8e3dd5bb3fdb79e
Kiril Gaydarov <kvg1@alu.ua.es>
2018-10-22 20:18:40 +0200

    Traducido en inglés

--- index.js
+++ index.js
@@ -6,162 +6,152 @@ app.use(bodyParser.json())
 var knex = require('knex')({
     client: 'sqlite3',
     connection: {
-      filename: "./coches.db"
+      filename: "./cars.db"
     },
     
     useNullAsDefault: true
 });
 
 /*
-* "capa" web (no aparecen referencias al API de Knex)
+* WEB LAYER
 */
-app.get("/", function(pet, resp) {
-    resp.redirect('/cars')
+app.get("/", function(req, res) {
+    res.redirect('/cars')
 })
 
 //API: get all cars
-app.get("/cars", function(pet, resp){
-    listarCoches(function(datos){
-        resp.send(datos)
+app.get("/cars", function(req, res){
+    listCars(function(data){
+        res.send(data)
     })
 
 })
 
 //API: get car by ID
-app.get("/cars/:id", function(pet, resp){
-    listarCocheID(function(datos){
-        resp.send(datos)
-    }, pet.params.id)
+app.get("/cars/:id", function(req, res){
+    listCarsByID(function(data){
+        res.send(data)
+    }, req.params.id)
 
 })
 
 //API: get all available cars
-app.get("/available", function(pet, resp){
-    listarCochesDisponibles(function(datos) {
-        resp.send(datos)
+app.get("/available", function(req, res){
+    listAvailableCars(function(data) {
+        res.send(data)
     })
 })
 
-//API: get all available cars from a given year
-app.get("/available/:year", function(pet, resp) {
-    listarCochesDisponiblesPorAnyo(function(datos) {
-        resp.send(datos)
-    }, pet.params.year)
+//API: get all available cars greater than a given year
+app.get("/available/:year", function(req, res) {
+    listAvailableCarsByYear(function(data) {
+        res.send(data)
+    }, req.params.year)
 })
 
 //API: delete car by ID
-app.delete("/cars/:id", function(pet, resp){
-    deleteCocheID(function(mensaje){
-        resp.send(mensaje)
-    }, pet.params.id)
+app.delete("/cars/:id", function(req, res){
+    deleteCarByID(function(message){
+        res.send(message)
+    }, req.params.id)
 })
 
 //API: post car by ID
-app.post("/cars", function(pet, resp) {
-    crearCoche(function(mensaje){
-        resp.send(mensaje)
-    }, pet.body)
+app.post("/cars", function(req, res) {
+    createCar(function(message){
+        res.send(message)
+    }, req.body)
 })
 
 //API: put car by ID
-app.put("/cars", function(pet, resp) {
-    editarCoche(function(mensaje){
-        resp.send(mensaje)
-    }, pet.body)
+app.put("/cars", function(req, res) {
+    editCar(function(message){
+        res.send(message)
+    }, req.body)
 })
 
 /*
-* "capa" de acceso a datos (no aparecen referencias al API de Express)
+* PERSISTANCE LAYER
 */
-//Lista todos los coches
-function listarCoches(callback) {
-    knex.select().from('Coches')
-    .then(function(datos){
-        console.log("Listando todos los coches")
+//List all cars
+function listCars(callback) {
+    knex.select().from('Cars')
+    .then(function(data){
+        console.log("Showing all cars")
 
-        callback(datos)
+        callback(data)
     })
 }
 
-//Lista un coche dado su ID
-function listarCocheID(callback, id) {
-    knex.select().from('Coches').where('id', id)
-    .then(function(datos){
-        console.log("Listando coche con ID: " + id)
+//List a car by its ID
+function listCarsByID(callback, id) {
+    knex.select().from('Cars').where('id', id)
+    .then(function(data){
+        console.log("Showing car with ID: " + id)
 
-        callback(datos)
+        callback(data)
     })
 }
 
-//Lista todos los coches disponibles
-function listarCochesDisponibles(callback) {
-    knex.select().from('Coches').where('disponible', 'si')
-    .then(function(datos){
-        console.log("Listando todos los coches disponibles")
+//List all available cars
+function listAvailableCars(callback) {
+    knex.select().from('Cars').where('available', 'yes')
+    .then(function(data){
+        console.log("Showing all available cars")
 
-        callback(datos)
+        callback(data)
     })
 }
 
-//Lista todos los coches disponibles a partir de un año
-function listarCochesDisponiblesPorAnyo(callback, year) {
-    knex('Coches').where('disponible', 'si')
-        .andWhere('fecha_matriculacion', '>=', year)
-        .then(function(datos) {
-            console.log("Listando coches disponibles a partir del año " + year)
+//List all available cars greater than a given year
+function listAvailableCarsByYear(callback, year) {
+    knex('Cars').where('available', 'yes')
+        .andWhere('year', '>=', year)
+        .then(function(data) {
+            console.log("Showing all avaialbe cars made after " + year)
 
-            callback(datos)
+            callback(data)
         })
 }
 
-//Lista todos los coches a partir de un año dado
-function listarCochesDisponibles(callback) {
-    knex.select().from('Coches').where('disponible', 'si')
-    .then(function(datos){
-        console.log("Listando todos los coches disponibles")
-
-        callback(datos)
-    })
-}
-
-//Borra un coche dado su ID
-function deleteCocheID(callback, id) {
-    knex('Coches').where('id', id).del()
+//Delete a car given its ID
+function deleteCarByID(callback, id) {
+    knex('Cars').where('id', id).del()
     .then(function(){
-        console.log("Borrando coche: " + id)
+        console.log("Deleted car: " + id)
 
-        callback("Borrado exitosamente!")
+        callback("Successfully deleted!")
     })
 }
 
-//Crea un coche nuevo
-function crearCoche(callback, coche) {
-    knex('Coches').insert(coche).then(function (mensaje) {
-        console.log("Insertando coche:")
-        console.log("  -"  + coche.marca)
-        console.log("  -"  + coche.modelo)
-        console.log("  -"  + coche.fecha_matriculacion)
-        console.log("  -"  + coche.pais_fabricacion)
+//Insert a new car
+function createCar(callback, car) {
+    knex('Cars').insert(car).then(function (message) {
+        console.log("Inserted car:")
+        console.log("  -"  + car.maker)
+        console.log("  -"  + car.model)
+        console.log("  -"  + car.year)
+        console.log("  -"  + car.country)
 
-        callback("Insertado correctamente")
+        callback("Successfully inserted")
     })
 }
 
-//Edita un coche existente
-function editarCoche(callback, coche) {
-    knex('Coches').where('id', coche.id).update(coche).then(function (mensaje) {
-        console.log("Editando coche:")
-        console.log("  -"  + coche.id)
-        console.log("  -"  + coche.marca)
-        console.log("  -"  + coche.modelo)
-        console.log("  -"  + coche.fecha_matriculacion)
-        console.log("  -"  + coche.pais_fabricacion)
+//Edit an existing car
+function editCar(callback, car) {
+    knex('Cars').where('id', car.id).update(car).then(function (message) {
+        console.log("Edited car:")
+        console.log("  -"  + car.id)
+        console.log("  -"  + car.maker)
+        console.log("  -"  + car.model)
+        console.log("  -"  + car.year)
+        console.log("  -"  + car.country)
 
-        callback("Editado correctamente")
+        callback("Successfully edited")
     })
 }
 
-//Pone el servidor en marcha
+//Start the server
 app.listen(3000, function(){
-    console.log("Esperando peticiones por el puerto 3000")
+    console.log("Waiting for connections through the port 3000")
 })
